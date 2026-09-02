c5691bcfd3f0c8feec87f172f213122251cb4de5
PeterSuh-Q3 <dante9000@gmail.com>
2026-09-02 15:49:50 +0900

    fix: advertise DSM platform identifiers for universal monitor package

--- scripts/build-monitor-spk.sh
+++ scripts/build-monitor-spk.sh
@@ -8,6 +8,9 @@ ROOT=$(cd "$(dirname "$0")/.." && pwd)
 PACKAGE=syno-nvidia-gpu-monitor
 VERSION=0.6.2
 PLATFORM=x86_64
+# Synology validates arch against model platform identifiers. This package is
+# userspace-only, so advertise all supported x86 platforms in INFO.
+DSM_ARCHES="apollolake broadwell broadwellnk broadwellnkv2 broadwellntbap geminilake purley r1000 v1000 epyc7002 epyc7003 epyc7003ntb geminilakenk icelaked r1000nk v1000nk kvmx64"
 IMAGE=${SYNOCOMPILER_IMAGE:-dante90/syno-compiler:7.4}
 CC=${SYNOCOMPILER_CC:-/opt/epyc7002/bin/x86_64-pc-linux-gnu-gcc}
 WORK="$ROOT/work/$PACKAGE-$PLATFORM"
@@ -38,6 +41,8 @@ cp "$ROOT/monitor-spk/webui/config" "$WORK/target/ui/config"
 cp "$ROOT/monitor-spk/webui/dsm-wrapper.js" "$WORK/target/ui/dsm-wrapper.js"
 cp "$ROOT/spk/PACKAGE_ICON_256.PNG" "$WORK/target/ui/images/icon_256.png"
 cp "$ROOT/monitor-spk/INFO" "$WORK/INFO"
+DSM_ARCHES="$DSM_ARCHES" awk '!/^arch=/{print; next} {print "arch=\"" ENVIRON["DSM_ARCHES"] "\""}' "$WORK/INFO" > "$WORK/INFO.tmp"
+mv "$WORK/INFO.tmp" "$WORK/INFO"
 for icon in PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG; do
   cp "$ROOT/spk/$icon" "$WORK/$icon"
 done
